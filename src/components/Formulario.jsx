067a126e9b93fb18f6bc128a946efad26ae5387d
Pedro Henrique <pedrohdlgomes@gmail.com>
2025-09-09 19:52:33 -0300

    feat: cria componente formulario
    criação do componente Formulario, referente às telas de Login, Cadastro e Esqueci Senha

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,44 +2,48 @@ import React from "react";
 
 const Formulario = ({ titulo, subtitulo, children }) => {
   return (
-    <div className="flex w-full h-screen">
-      {/* Lado direito com formulário */}
-      <div className="w-6 flex flex-column justify-content-center align-items-center">
-        <div className="w-10 md:w-6">
-          <h1
-            style={{
-              fontSize: "1.8rem",
-              color: "#f97316",
-              fontWeight: "700",
-              textAlign: "center",
-              marginBottom: "0.5rem",
-              fontFamily: "'Poppins', sans-serif", 
-            }}
-          >
-            {titulo}
-          </h1>
-          <p
-            style={{
-              color: "#6b7280",
-              textAlign: "center",
-              marginBottom: "1rem",
-              fontFamily: "'Poppins', sans-serif",
-            }}
-          >
-            {subtitulo}
-          </p>
-
-          {/* Linha separadora entre subtítulo e inputs */}
-          <hr
-            style={{
-              border: "none",
-              borderTop: "1px solid #2b2c2eff",
-              marginBottom: "1.5rem",
-            }}
-          />
-          {children}
-        </div>
+    <div style={{ width: "100%", maxWidth: "400px" }}>
+      <div style={{ textAlign: "center", marginBottom: "16px" }}>
+        <h1
+          style={{
+            fontSize: "1.8rem",
+            color: "#f97316",
+            fontWeight: "700",
+            marginBottom: "0.3rem",
+            fontFamily: "'Inter', sans-serif",
+          }}
+        >
+          {titulo}
+        </h1>
+        <p
+          style={{
+            color: "#6b7280",
+            fontSize: "1rem",
+            fontFamily: "'Inter', sans-serif",
+          }}
+        >
+          {subtitulo}
+        </p>
       </div>
+
+      <hr
+        style={{
+          border: "0",
+          height: "1px",
+          backgroundColor: "#78797bff",
+          margin: "1rem 0 1.5rem 0",
+        }}
+      />
+
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "14px",
+        }}
+      >
+        {children}
+      </form>
     </div>
   );
 };
