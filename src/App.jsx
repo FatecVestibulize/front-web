96b58f56739f68676f9c4353a20180bc395407be
Felipe Lancho <felipemarin46@gmail.com>
2025-09-09 20:18:47 -0300

    fix: deixa app neutro

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import RedirecionamentoLinks from "./components/RedirecionamentoLinks"
-
 function App() {
 
   return (
     <>
-    <RedirecionamentoLinks links={[
-          {prefix: "Caso não tenha cadastro, ", to:"/cadastro", label:"clique aqui", linkStyle:{}},
-          {prefix: "Caso tenha cadastro, ", to:"/login", label:"clique aqui", linkStyle:{}}
-        ]}/>
     </>
   )
 }
