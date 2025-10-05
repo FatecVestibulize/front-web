4879046c2b9a98727fc7620e514fbc39d22d1b83
Felipe Lancho <felipemarin46@gmail.com>
2025-10-05 12:25:26 -0300

    fix: altera o funcionamento do banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,48 +1,55 @@
-import logo from "../assets/logo.png";
+import logo from "../assets/logo-isolada.png";
 import imagemBanner from "../assets/imagem-banner.png";
 
 export default function Banner() {
   return (
-    
     <div
       style={{
         position: "relative",
         width: "50vw",
         height: "100vh",
         overflow: "hidden",
-        display: "flex",
-        flexDirection: "column",
-        position: "absolute",
-        height: "100%",
-        left: "0px",
-        top: "0px",
-        background: "linear-gradient(143.35deg, #FFC514 68.63%, #000000 166.73%)",
+        backgroundColor: "#FFC514", // fundo amarelo base
       }}
     >
-      {/* Logo no topo */}
+      {/* Faixa roxa inclinada (topo) */}
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "42%",
+          backgroundColor: "#3d366c",
+          clipPath: "polygon(0 0, 100% 0, 100% 30%, 0 44%)",
+          zIndex: 0,
+        }}
+      />
+
+      {/* Logo limpa (transparente) */}
       <img
         src={logo}
         alt="Logo Vestibulize"
         style={{
-          width: "100%",
-          height: "auto",
-          display: "block",    
-          objectFit: "cover",
+          width: 260,
+          margin: 28,
+          position: "relative",
+          zIndex: 2,
         }}
       />
 
+      {/* Juninha */}
       <img
         src={imagemBanner}
         alt="Estudante"
         style={{
           position: "absolute",
+          bottom: 0,
           left: "50%",
-          bottom: 0, 
-          width: "100%",                  
           transform: "translateX(-50%)",
-          maxWidth: "100%",
+          maxWidth: "95%",
           height: "auto",
-          display: "block",
+          zIndex: 1,
           pointerEvents: "none",
           userSelect: "none",
         }}
