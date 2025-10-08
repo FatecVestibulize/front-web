01b51a11f1462521d3e9f8355701cf878da35214
Gabriel Carvalho <121682225+gabrielrenato1@users.noreply.github.com>
2025-10-08 11:00:02 -0300

    fix: ajusta erro de conflito

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
