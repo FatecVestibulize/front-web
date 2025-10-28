ea94cdabf8d852a5828b457e7de98f214f5754f0
Gabriel Carvalho <121682225+gabrielrenato1@users.noreply.github.com>
2025-10-27 21:45:19 -0300

    feat: Cria pagina de anotacao

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Cadastro from "./pages/Cadastro";
 import EsqueciSenha from "./pages/EsqueciSenha";
 import Navbar from "./components/Navbar";
 import Caderno from "./pages/Caderno";
+import Anotacao from "./pages/Anotacao";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
           <Route path="/esqueci-senha" element={<EsqueciSenha />} />
+          <Route path="/caderno/:notebook_id/anotacoes" element={ <Anotacao /> } />
           <Route path="/caderno" element={<Caderno />} />
         </Routes>
       </div>
